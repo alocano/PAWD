b049519ea667a8e8df8696a3f7068d3640914ab1
JN-link <jngugi@csus.edu>
2025-11-19 13:48:24 -0800

    Update main.c

--- FingerTap/Src/main.c
+++ FingerTap/Src/main.c
@@ -24,6 +24,7 @@
 #include "Adafruit_VCNL4020.h"
 #include "tap.h"
 #include <stdio.h>
+#include <string.h>
 extern SensorBuffer_t pre_capture_buffer;
 extern SensorBuffer_t post_capture_buffer;
 /* USER CODE END Includes */
@@ -52,6 +53,11 @@ I2C_HandleTypeDef hi2c2;
 
 UART_HandleTypeDef huart3;
 
+// Serial transmission buffer
+char txBuffer[50];
+uint32_t lastSendTime = 0;
+const uint32_t SEND_INTERVAL = 100; // ms
+
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -77,8 +83,11 @@ static void MX_USART3_UART_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin);
+void sendProximityData(VCNL4020_HandleTypeDef *dev);
 
 VCNL4020_HandleTypeDef dev;
+
+int count = 0;
 
 
 PUTCHAR_PROTOTYPE {
@@ -90,17 +99,25 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
     if (GPIO_Pin == TAP_GPIO_Pin) {
         if (HAL_GPIO_ReadPin(TAP_GPIO_Port, TAP_GPIO_Pin) == GPIO_PIN_RESET) {
-            // Switch pressed - start post-capture
-            pre_capture_buffer.capturing = false;
-            post_capture_buffer.capturing = true;
-            post_capture_buffer.write_index = 0;
-        } else {
-            // Switch released - stop capturing
-            post_capture_buffer.capturing = false;
-        }
+        	count ++;
     }
 }
 
+
+void sendProximityData(VCNL4020_HandleTypeDef *dev) {
+    if (HAL_GetTick() - lastSendTime >= SEND_INTERVAL) {
+        // Read proximity data
+        uint16_t proximity = VCNL4020_ReadProximity(dev);
+
+        // Format: "PROXIMITY,<timestamp>,<value>"
+        sprintf(txBuffer, "PROXIMITY,%lu,%u\n", HAL_GetTick(), proximity);
+
+        // Send via UART
+        HAL_UART_Transmit(&huart3, (uint8_t*)txBuffer, strlen(txBuffer), HAL_MAX_DELAY);
+
+        lastSendTime = HAL_GetTick();
+    }
+}
 
 
 
@@ -142,16 +159,19 @@ int main(void)
   /* USER CODE BEGIN 2 */
 
 
-  if (!VCNL4020_Init(&dev, &hi2c2)) {
+  /*if (!VCNL4020_Init(&dev, &hi2c2)) {
       printf("Failed to initialize VCNL4020!\r\n");
   }
 
   else{
 	  printf("VCNL4020 initialized successfully\r\n");
-	  StartPreCapture();
   }
+  */
 
 
+
+
+/*
 // Try different proximity rates, faster measurements use more power of course!
 // Can also try: PROX_RATE_1_95_PER_S, PROX_RATE_3_9_PER_S, PROX_RATE_7_8_PER_S,
 // PROX_RATE_16_6_PER_S, PROX_RATE_31_2_PER_S, PROX_RATE_62_5_PER_S,
@@ -186,7 +206,7 @@ switch (VCNL4020_GetProxFrequency(&dev)) {
   case PROX_FREQ_781_25_KHZ: printf("781.25 KHz"); break;
   case PROX_FREQ_1_5625_MHZ: printf("1.5625 MHz"); break;
   case PROX_FREQ_3_125_MHZ: printf("3.125 MHz"); break;
-}
+}*/
 
 /*
 printf("Interrupt Status: %u\r\n", VCNL4020_GetInterruptStatus(&dev));
@@ -195,6 +215,7 @@ printf("Interrupt Status: %u\r\n", VCNL4020_GetLowThreshold(&dev));
 
 printf("Interrupt Status: %u\r\n", VCNL4020_GetHighThreshold(&dev));
 */
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -205,10 +226,8 @@ printf("Interrupt Status: %u\r\n", VCNL4020_GetHighThreshold(&dev));
 
     /* USER CODE BEGIN 3 */
 	  if(VCNL4020_IsProxReady(&dev)){
-	        PreCaptureTask(&dev);
-	        PostCaptureTask(&dev);
+		  sendProximityData(&dev);
 	  }
-	  ProcessCapturedData();
 
 	  VCNL4020_clearInterrupts(&dev, true, false, false, false);
 /*
