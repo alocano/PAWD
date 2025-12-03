a5f800a8a51635d3828f88c244b37655070fdd8c
Jason Mansyur <jasoncorv@gmail.com>
2025-12-03 14:24:39 -0800

    MatLab Files reorganized

---
+++ MatLab/Proximity_v4.m
@@ -0,0 +1,33 @@
+    
+    % Annotate each interrupt point
+    for i = 1:length(interruptTimes)
+        text(interruptTimes(i), interruptProximity(i), ...
+             sprintf('  Int #%d', i), ...
+             'VerticalAlignment', 'bottom', 'FontSize', 10, ...
+             'FontWeight', 'bold', 'Color', 'red');
+    end
+    
+    xlabel('Time (s)');
+    ylabel('Proximity');
+    title('Final Plot: VCNL4020 Proximity Data with GPIO Interrupt Markers');
+    grid on;
+    legend('show');
+    
+    % Save data to file with interrupt information
+    dataTable = table(timeData', proximityData', 'VariableNames', {'Time_s', 'Proximity'});
+    writetable(dataTable, 'proximity_data.csv');
+    
+    % Save interrupt times separately
+    interruptTable = table((1:length(interruptTimes))', interruptTimes', interruptProximity', ...
+        'VariableNames', {'Interrupt_Number', 'Time_s', 'Proximity_Value'});
+    writetable(interruptTable, 'interrupt_times.csv');
+    
+    fprintf('Data saved to proximity_data.csv\n');
+    fprintf('Interrupt times saved to interrupt_times.csv\n');
+    
+catch ME
+    fprintf('Error: %s\n', ME.message);
+end
+
+% Clean up
+clear s;
